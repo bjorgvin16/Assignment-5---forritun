5001ae9c0bfc8808884f495e3cca4533cc5ecb6a
Björgvin Elisson <bjorgvinelisson@Bjorgvins-MacBook-Pro-2.local>
2019-09-05 09:57:40 +0000

    Algorithm written for max_int.py

--- max_int.py
+++ max_int.py
@@ -1,1 +1,7 @@
-# 
+# --------- ALGORTIHM ---------
+# 1. Ask user to input a number
+# 2. Ask user to input another number
+# 3. Compare the two inputs and determine which number is larger. Define the largest number with a variable
+# 4. Repeat steps two and three until user inputs a negative number
+# 5. Print largest number
+# --------- ALGORTIHM ---------

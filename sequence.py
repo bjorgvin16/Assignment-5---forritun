398c04ff811fa40b6d98e1577b0ccbb1ddbba60d
Björgvin Elisson <bjorgvinelisson@Bjorgvins-MacBook-Pro-2.local>
2019-09-05 11:20:07 +0000

    Algorithm sligthly changed, first draft of code

--- sequence.py
+++ sequence.py
@@ -3,9 +3,22 @@
 # 2. For fourth number in sequnce, add previous three numbers. 
 #    For fifth number in sequence, add previous three numbers and so on. 
 # 3. Ask user for an input n determining how many numbers from the sequnce, the program should print.
+# 4. Print first three numbers of sequence outside of loop
 # 4. Create a loop that:
-#       Prints each number of the sequence
+#       Prints each number of the sequence except first three numbers
 #       Changes the variables for the previous 3 numbers each loop
 #       Adds previous three numbers each loop to find next number in sequence
 #       Stops when n many numbers of sequence have been printed
 # --------- ALGORTIHM ---------
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+first_num = 1
+second_num = 2
+third_num = 3
+
+print(first_num)
+print(second_num)
+print(third_num)
+
+counter = 3 # counter starts at 3 because first three numbers of sequencd have already been defined
+

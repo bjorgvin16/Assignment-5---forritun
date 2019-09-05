4b98990a215494e8ee2a54a95c929b65e6ac4bf0
Björgvin Elisson <bjorgvinelisson@Bjorgvins-MacBook-Pro-2.local>
2019-09-05 10:17:32 +0000

    First test, if correct, no more commits will follow

--- max_int.py
+++ max_int.py
@@ -3,19 +3,18 @@
 # 2. Assign the first input number to a variable defining it as the greatest number
 # 3. Ask user to input another number
 # 4. Compare the second number to the greatest number and define the greater number as the greatest number
-# 4. Repeat steps three and four until user inputs a negative number
-# 5. Print largest number
+# 5. Repeat steps three and four until user inputs a negative number
+# 6. Print largest number
 # --------- ALGORTIHM ---------
 
 num_int = int(input("Input a number: "))    # Do not change this line
 # Fill in the missing code
-num_int2 = int(input("Input another number: "))
-max_int = 0
+max_int = num_int
+another_num_int = 0
 
-while num_int:
-    if num_int >= num_int2:
-        greatest_num = num_int
-    else:
-        greatest_num = num_int2
+while another_num_int >= 0:
+    another_num_int = int(input("Input a number: "))
+    if another_num_int > max_int:
+        max_int = another_num_int
 
 print("The maximum is", max_int)    # Do not change this line

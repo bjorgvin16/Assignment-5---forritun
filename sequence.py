ac277f8de4677fbac6f5d3d75da85c134d6ee769
Björgvin Elisson <bjorgvinelisson@Bjorgvins-MacBook-Pro-2.local>
2019-09-05 10:58:21 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,11 @@
+# --------- ALGORTIHM ---------
+# 1. First three numbers in sequence are 1, 2, and 3.
+# 2. For fourth number in sequnce, add previous three numbers. 
+#    For fifth number in sequence, add previous three numbers and so on. 
+# 3. Ask user for an input n determining how many numbers from the sequnce, the program should print.
+# 4. Create a loop that:
+#       Prints each number of the sequence
+#       Changes the variables for the previous 3 numbers each loop
+#       Adds previous three numbers each loop to find next number in sequence
+#       Stops when n many numbers of sequence have been printed
+# --------- ALGORTIHM ---------
